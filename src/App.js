3e4371f494954e3fa9bfbb1c4f399a86771a930c
kianaamid <kiana.amid@gmail.com>
2023-08-22 10:27:20 +1000

    Add json-server(API)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MovieList from "./component/MovieList/MovieList";
 import AddMovie from "./component/AddMovie/AddMovie";
 
 function App() {
-  const [movie, setMovie] = useState([
-    {
-      id: 1,
-      name: "Barbie",
-      price: 20,
-    },
-    {
-      id: 2,
-      name: "Oppenheimer",
-      price: 20,
-    },
-    {
-      id: 3,
-      name: "Mission Impossible",
-      price: 20,
-    },
-  ]);
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const response = await fetch("http://localhost:8001/movies");
+      const responseData = await response.json();
+      setMovie(responseData);
+    };
+    sendRequest();
+  }, []);
 
-  const deleteMovie = (id) => {
+  const deleteMovie = async (id) => {
+    await fetch(`http://localhost:8001/movies/${id}`, { method: "DELETE" });
     setMovie(movie.filter((movie) => movie.id !== id));
   };
 
-  const addMovie = (name, price) => {
-    const id = Math.floor(Math.random() * 10000);
-    const newMovie = { id, ...name, ...price };
-    console.log(newMovie);
-    console.log(...movie);
-    setMovie([...movie, newMovie]);
+  const addMovie = async (name, price) => {
+    const response = await fetch("http://localhost:8001/movies", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(name, price),
+    });
+    const responseData = await response.json();
+    setMovie([...movie, responseData]);
   };
   return (
     <div className="App">
